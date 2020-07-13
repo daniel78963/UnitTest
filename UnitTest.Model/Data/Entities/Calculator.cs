81303019c475a9774298111393d5288b2ba0a018
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2020-07-13 16:20:41 -0500

    TestCalculatorAdd

--- UnitTest.Model/Data/Entities/Calculator.cs
+++ UnitTest.Model/Data/Entities/Calculator.cs
@@ -7,5 +7,9 @@ namespace UnitTest.Model.Data.Entities
 
     public class Calculator
     {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
     }
 }
--- UnitTest.UnitTestModel/CalculatorUnitTest.cs
+++ UnitTest.UnitTestModel/CalculatorUnitTest.cs
@@ -2,13 +2,20 @@
 namespace UnitTest.UnitTestModel
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using UnitTest.Model.Data.Entities;
 
     [TestClass]
     public class CalculatorUnitTest
     {
+        [TestMethod]
         public void TestCalculatorAdd()
         {
-            //Calculator
+            Calculator calculator = new Calculator();
+
+            int num1 = 3, num2 = 8;
+            var result = calculator.Add(num1, num2);
+
+            Assert.AreEqual(num1 + num2, result);
         }
     }
 }
